c747c4a15b49bc60f7b193ded2e8e952d4e9151c
David Nitu <davidnitu44@gmail.com>
2022-10-04 15:00:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = "David";
+let lastName = "Nitu";
+let fullName = name + " " + lastName;
+
+console.log("Hi, my name is " + fullName)
+
+// *******************
+let age = 21;
+let currYear = 2022;
+
+console.log("Birth Year: " + (currYear - age))
+
+
+
